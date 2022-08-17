5daaf1adda46ff16393ec81d2cf0d836d1309a97
lhf <626712309@qq.com>
2022-08-17 19:03:36 +0800

    feat：新增检查版本号功能

--- packages/core/bin/index.js
+++ packages/core/bin/index.js
@@ -8,5 +8,5 @@ if (importLocal(__filename)) {
   require('npmlog').info('提示', '正在使用当前项目中 wheel-cli 的版本')
 } else {
   // 使用全局下的脚手架命令
-  require('../lib')
+  require('../lib')(process.argv.slice(2))
 }
--- packages/core/lib/index.js
+++ packages/core/lib/index.js
@@ -1,6 +1,28 @@
 'use strict';
 
+// 引入当前脚手架的 package.json
+const pkg = require('../package.json')
+
 // 引入我们封装的 npmlog 工具
 const log = require('@wheel-cli/log')
 
-log.success('test', 'success.......')
+/**
+ * @description: 核心方法
+ * @param {*}
+ * @return {*}
+ */
+function core() {
+    // 检查版本号
+    checkPkgVersion()
+}
+
+/**
+ * @description: 检查 package.json 中的 version 版本号
+ * @param {*}
+ * @return {*}
+ */
+function checkPkgVersion() {
+    log.success('友情提示，当前的版本是：', pkg.version)
+}
+
+module.exports = core
