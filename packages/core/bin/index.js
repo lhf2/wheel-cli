ebc2d03ffd8005c2b5d4ccc6334bab8ae6b00798
lhf <626712309@qq.com>
2022-08-17 18:46:15 +0800

    feat：新增定制log样式功能

--- packages/core/bin/index.js
+++ packages/core/bin/index.js
@@ -1,14 +1,12 @@
 #! /usr/bin/env node
 
-// const importLocal = require('import-local')
+const importLocal = require('import-local')
 
-// // 如果当前项目中的 node_modules 中存在一个脚手架命令，全局的 node 环境中也存在一个脚手架命令的时候
-// // import-local 会优先选用项目中 node_modules 的版本，然后输出 log
-// if (importLocal(__filename)) {
-//   require('npmlog').info('提示', '正在使用当前项目中 wheel-cli 的版本')
-// } else {
-//   // 使用全局下的脚手架命令
-//   require('../lib')(process.argv.slice(2))
-// }
-
-console.log(1111111111111111);
+// 如果当前项目中的 node_modules 中存在一个脚手架命令，全局的 node 环境中也存在一个脚手架命令的时候
+// import-local 会优先选用项目中 node_modules 的版本，然后输出 log
+if (importLocal(__filename)) {
+  require('npmlog').info('提示', '正在使用当前项目中 wheel-cli 的版本')
+} else {
+  // 使用全局下的脚手架命令
+  require('../lib')
+}
